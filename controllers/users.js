3f10ea40df33075595961456c540c5f16b9fe318
KARIMOV <marat.karimov@icloud.com>
2021-08-29 19:54:24 +0400

    fix: auth

--- controllers/users.js
+++ controllers/users.js
@@ -107,9 +107,9 @@ module.exports.login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
       res.cookie('jwt', token, {
-        maxAge: 3600000,
+        maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        sameSite: 'none',
+      //  sameSite: 'none',
       })
         .send({
           user: {
