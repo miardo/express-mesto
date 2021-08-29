ae71bab3abbb50e73d1f8797df20b3ae60bbff93
KARIMOV <marat.karimov@icloud.com>
2021-08-29 20:09:23 +0400

    fix: auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,16 +5,17 @@ const jwt = require('jsonwebtoken');
 const E401 = require('./E401');
 
 module.exports = (req, res, next) => {
-  if (!req.cookies.jwt) {
+  const token = req.cookies.jwt;
+  const { NODE_ENV, JWT_SECRET } = process.env;
+
+  if (!token) {
     next(new E401('Необходима авторизация.'));
   }
 
-  const token = req.cookies.jwt;
-  const { NODE_ENV, JWT_SECRET } = process.env;
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     next(new E401('Необходима авторизация.'));
   }
